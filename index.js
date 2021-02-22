6c41fc1a1a4f499ce30bc363b1c944cd1618c263
Noah Herrin <herrinn@wwu.edu>
2021-02-22 13:51:58 -0800

    fixed links

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ function checkFileType(file, cb){
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 app.get('/', (req, res) => res.render('index',{msg: "Hello, World!"}));
+app.get('/searchYourself.ejs', (req, res) => res.render('searchYourself'))
 app.post('/upload', (req, res) =>{
         upload(req, res, (err) => {
             if(err){
